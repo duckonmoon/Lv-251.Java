390a7d444c99e1eef941b4071b49bc6c377bedc8
Pavlo Kuchereshko <atos.ua@gmail.com>
2017-07-12 00:13:22 +0300

    Update Clinics.java
    fix

--- src/main/java/entity/Clinics.java
+++ src/main/java/entity/Clinics.java
@@ -9,19 +9,9 @@ import javax.persistence.Entity;
 
 @Entity
 public class Clinics extends BaseEntity {
-    private long id_clinic;
     private String clinic_name;
-    private long id_contact;
 
     public Clinics() {
-    }
-
-    public long getId_clinic() {
-        return id_clinic;
-    }
-
-    public void setId_clinic(long id_clinic) {
-        this.id_clinic = id_clinic;
     }
 
     public String getClinic_name() {
@@ -30,13 +20,5 @@ public class Clinics extends BaseEntity {
 
     public void setClinic_name(String clinic_name) {
         this.clinic_name = clinic_name;
-    }
-
-    public long getId_contact() {
-        return id_contact;
-    }
-
-    public void setId_contact(long id_contact) {
-        this.id_contact = id_contact;
     }
 }

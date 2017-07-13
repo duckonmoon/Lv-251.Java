8137beaa73f74e8b105355a593f650d340b07d05
kilopowq <marian.brynetskyy@outlook.com>
2017-07-13 14:12:04 +0300

    added basedao

---
+++ src/main/java/com/softserve/edu/lv251/dao/BaseDAO.java
@@ -0,0 +1,20 @@
+package com.softserve.edu.lv251.dao;
+
+import java.util.List;
+
+/**
+ *
+ */
+public interface BaseDAO<T> {
+    void addEntity(T entity);
+
+    void updateEntity(T entity);
+
+    T getEntityByID(Integer entityId);
+
+    List<T> getEntitiesByColumnNameAndValue(String columnName, Object value);
+
+    List<T> getAllEntities();
+
+    void deleteEntity(T entity);
+}

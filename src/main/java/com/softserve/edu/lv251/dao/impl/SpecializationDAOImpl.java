cc078814f7e8689a356d8336aa859e54bcb9cdfd
EmptyError <empty404error404@gmail.com>
2017-07-21 16:00:49 +0300

    add Specialization to Doctors,add doctorsService

---
+++ src/main/java/com/softserve/edu/lv251/dao/impl/SpecializationDAOImpl.java
@@ -0,0 +1,10 @@
+package com.softserve.edu.lv251.dao.impl;
+
+import com.softserve.edu.lv251.dao.SpecializationDAO;
+import com.softserve.edu.lv251.entity.Specialization;
+
+/**
+ * Created by Admin on 21.07.2017.
+ */
+public class SpecializationDAOImpl extends BaseDAOImpl<Specialization> implements SpecializationDAO {
+}

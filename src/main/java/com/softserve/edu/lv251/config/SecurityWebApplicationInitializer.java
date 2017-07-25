9ed880a4125d7c1a5ef64dd454a0eab8a45632e9
kilopowq <marian.brynetskyy@outlook.com>
2017-07-25 12:44:24 +0300

    first login

---
+++ src/main/java/com/softserve/edu/lv251/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.softserve.edu.lv251.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by kilopo on 25.07.2017.
+ */
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+}
--- src/main/java/com/softserve/edu/lv251/config/WebSecurityConfig.java
+++ src/main/java/com/softserve/edu/lv251/config/WebSecurityConfig.java
@@ -5,12 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -23,10 +26,39 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() { return new BCryptPasswordEncoder();}
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(customUserDetailsService);
+//    }
+
+
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailsService);
+    protected void configure(HttpSecurity http) throws Exception {
+            http.csrf()
+                    .disable()
+                    .authorizeRequests()
+                    .antMatchers("/resources/**", "/**").permitAll()
+                    .anyRequest().permitAll()
+                    .and();
+
+            http.formLogin()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/j_spring_security_check")
+                    .failureUrl("/clinics")
+                    .usernameParameter("j_username")
+                    .passwordParameter("j_password")
+                    .permitAll();
+
+            http.logout()
+                    .permitAll()
+                    .logoutUrl("/index")
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true);
     }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService);
+    }
 
 }

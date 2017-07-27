2a77b9fd73ee2d1a47614afee88ba3fa6170f5ae
Shmidt <vitaliy.kovalevskyy@gmail.com>
2017-07-27 20:34:05 +0300

    added doctor_cabinet.jsp

---
+++ src/main/java/com/softserve/edu/lv251/controllers/DoctorCabinetController.java
@@ -0,0 +1,17 @@
+package com.softserve.edu.lv251.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Author: Vitaliy Kovalevskyy
+ */
+@Controller
+public class DoctorCabinetController {
+    @RequestMapping(value = "/doctorCabinet",method = RequestMethod.GET)
+    public String home(ModelMap model){
+        return "doctor_cabinet";
+    }
+}

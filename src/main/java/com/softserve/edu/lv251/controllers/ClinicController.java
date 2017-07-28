3af3730cf7a4e5a576cc27aaae8caaecd6ab5762
taraspelypets <tarasp@outlook.com>
2017-07-29 00:39:28 +0300

    ClinicsController.clinicDetails(): replaced @RequestParam to @PathVariable

--- src/main/java/com/softserve/edu/lv251/controllers/ClinicController.java
+++ src/main/java/com/softserve/edu/lv251/controllers/ClinicController.java
@@ -34,8 +34,8 @@ public class ClinicController {
 
 
 
-    @RequestMapping(value = "/details", method = RequestMethod.GET)
-    public String clinicDetails(@RequestParam(name = "id", required = true)long id, Model model){
+    @RequestMapping(value = "/details/{id}", method = RequestMethod.GET)
+    public String clinicDetails(@PathVariable(name = "id", required = true)long id, Model model){
         Clinics clinic = clinicService.getClinicByID(id);
         model.addAttribute("clinic", clinic);
         model.addAttribute("mappoint", clinic.getContact().getAddress() + " " + clinic.getContact().getCity());

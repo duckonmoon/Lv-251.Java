b4feae441f67f79444e240f9628dd7f153e1080a
rasim <gera9008@gmail.com>
2017-08-09 17:37:45 +0300

    merge app heroku properties

--- src/main/java/com/softserve/edu/lv251/service/impl/AppointmentServiceImpl.java
+++ src/main/java/com/softserve/edu/lv251/service/impl/AppointmentServiceImpl.java
@@ -2,13 +2,15 @@ package com.softserve.edu.lv251.service.impl;
 
 import com.softserve.edu.lv251.config.Mapper;
 import com.softserve.edu.lv251.dao.AppointmentsDAO;
-import com.softserve.edu.lv251.dto.pojos.AppointmentsForCreationDTO;
 import com.softserve.edu.lv251.dto.pojos.AppointmentDTO;
+import com.softserve.edu.lv251.dto.pojos.AppointmentsForCreationDTO;
 import com.softserve.edu.lv251.entity.Appointments;
 import com.softserve.edu.lv251.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**

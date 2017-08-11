b80bf8737943655608e3ed44cc248173a1e9d13d
YanaMartynyak <empty404error404@gmail.com>
2017-08-11 13:36:50 +0300

    m

---
+++ src/main/java/com/softserve/edu/lv251/dto/pojos/UserToDoctor.java
@@ -0,0 +1,43 @@
+package com.softserve.edu.lv251.dto.pojos;
+
+/**
+ * Created by Admin on 10.08.2017.
+ */
+public class UserToDoctor {
+    private String email;
+    private String specialization;
+    private String clinicName;
+    private String description;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getClinicName() {
+        return clinicName;
+    }
+
+    public void setClinicName(String clinicName) {
+        this.clinicName = clinicName;
+    }
+}

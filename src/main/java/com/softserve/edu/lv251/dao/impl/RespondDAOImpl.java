49c41da40e8951dc902fe6ed2da0f7ed6d4f01e0
kilopowq <marian.brynetskyy@outlook.com>
2017-08-23 14:50:39 +0300

    respond serviceimpl

--- src/main/java/com/softserve/edu/lv251/dao/impl/RespondDAOImpl.java
+++ src/main/java/com/softserve/edu/lv251/dao/impl/RespondDAOImpl.java
@@ -1,9 +1,15 @@
 package com.softserve.edu.lv251.dao.impl;
 
+import com.softserve.edu.lv251.dao.RespondDAO;
 import com.softserve.edu.lv251.entity.Respond;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 /**
  * Created by Marian Brynetskyi on 23.08.2017.
  */
-public class RespondDAOImpl extends BaseDAOImpl<Respond> {
+@Transactional
+@Repository
+public class RespondDAOImpl extends BaseDAOImpl<Respond> implements RespondDAO {
 }
--- src/main/java/com/softserve/edu/lv251/service/RespondService.java
+++ src/main/java/com/softserve/edu/lv251/service/RespondService.java
@@ -1,7 +1,15 @@
 package com.softserve.edu.lv251.service;
 
+import com.softserve.edu.lv251.entity.Respond;
+
+import java.util.List;
+
 /**
  * Created by Marian Brynetskyi on 23.08.2017.
  */
 public interface RespondService {
+
+    List<Respond> getAllRespondsByDoctor(long doctorId);
+
+    List<Respond> getAllRespondsByUser(long userId);
 }

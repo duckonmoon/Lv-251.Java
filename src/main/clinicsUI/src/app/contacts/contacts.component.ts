db62bb96cc071894c083fc5c4fdd91e5e1472a51
kilopowq <marian.brynetskyy@outlook.com>
2017-09-02 18:27:36 +0300

    form

--- src/main/clinicsUI/src/app/contacts/contacts.component.ts
+++ src/main/clinicsUI/src/app/contacts/contacts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Http} from "@angular/http";
+
 
 @Component({
   selector: 'app-contacts',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.css']
 })
 export class ContactsComponent implements OnInit {
+  contactUsForm: FormGroup;
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
+    this.contactUsForm = new FormGroup ({
+      'name': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [ Validators.required, Validators.email]),
+      'phone': new FormControl(null, ),
+      'subject': new FormControl(null, Validators.required),
+      'message': new FormControl(null, Validators.required)
+    })
+
   }
 
+  onSubmit(){
+    console.log(this.contactUsForm)
+    this.contactUsForm.reset();
+  }
+
+  onSend(){
+    // http://192.168.101.30:8080/
+    this.http.post('http://10.4.48.181:8080/contact1', null).subscribe(
+      (response) => console.log(response),
+    );
+  }
 }

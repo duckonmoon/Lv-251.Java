85f86504e6aec2b971ecc62f1790122f0f22b4f8
Pavlo Kuchereshko <atos.ua@gmail.com>
2017-09-02 17:45:36 +0300

    aaaa

---
+++ src/main/clinicsUI/src/app/clinics/clinics-edit/clinics-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clinics-edit',
+  templateUrl: './clinics-edit.component.html',
+  styleUrls: ['./clinics-edit.component.css']
+})
+export class ClinicsEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
